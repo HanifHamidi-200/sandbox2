6d6ae0f1b685c362541a8770150a35f74bc4d3f0
Hanif Hamidi <Hanif@Melayu.org.uk>
2018-07-15 11:03:22 +0100

    routes.2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express=require('express');
 var router=express.Router();
 
-router.get('/tasks',function(req,res,next){
-    res.send('TASKS API');
+router.get('/',function(req,res,next){
+    res.send('INDEX PAGE');
 });
 
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,9 @@
 var express=require('express');
 var router=express.Router();
 
-router.get('/',function(req,res,next){
-    res.send('INDEX PAGE');
+router.get('/tasks',function(req,res,next){
+    res.send('TASKS API');
 });
 
 
 module.exports=router;
- 
